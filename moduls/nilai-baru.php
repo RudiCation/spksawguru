d539632f33bd727560bb3775bba40ad2368bd472
aktivitasRudi <rudiedukasih@gmail.com>
2024-02-18 19:04:19 +0700

    modif2

--- moduls/nilai-baru.php
+++ moduls/nilai-baru.php
@@ -3,6 +3,8 @@ include_once 'lib/nilai.inc.php';
 $eks = new Nilai($db);
 ?>
 <?php
+$get = $_GET['get'];
+if (empty($get)) {
 if($_POST){
 	$eks->nuptk = $_POST['nuptk'];
 	$eks->nama = $_POST['nama'];
@@ -70,6 +72,76 @@ if($_POST){
 
 		  </div>
 		  </div>
+		  <?php
+}elseif ($get == "edit") {
+	$id = isset($_GET['id']) ? $_GET['id'] : die('ERROR: missing ID.');
+	$eks->nuptk = $id;
+	$eks->readOne();
+?>
+<div class="container">
+		<div class="row">
+		  <div class="col-xs-12 col-sm-8 col-md-8">
+		  <div class="well">
+		  	<div class="page-header">
+			  <h3>Edit Nilai Preferensi</h3>
+			</div>
+			<form method="post">
+                <div class="form-group">
+				    <label for="kt">NUPTK</label>
+					<input type="number" class="form-control" readonly value="<?php echo $eks->nuptk; ?>" name="nuptk" id="nuptk"  >
+
+				  </div>
+				  <div class="form-group">
+				    <label for="kt">Nama Guru</label>
+				    <input type="text" class="form-control" value="<?php echo $eks->nama; ?>" id="nama" name="nama" required>
+				  </div>
+                  <div class="form-group">
+				    <label for="kt">Absensi</label>
+				    <input type="text" class="form-control" value="<?php echo $eks->absen; ?>" id="absen" name="absen" required>
+				  </div>
+                  <div class="form-group">
+				    <label for="kt">Kompetensi</label>
+				    <input type="text" class="form-control" value="<?php echo $eks->komptensi; ?>" id="komptensi" name="komptensi" required>
+				  </div>
+                  <div class="form-group">
+				    <label for="kt">Pendidikan</label>
+				    <input type="text" class="form-control" value="<?php echo $eks->pendidikan; ?>" id="pendidikan" name="pendidikan" required>
+				  </div>
+                  <div class="form-group">
+				    <label for="kt">Kinerja</label>
+				    <input type="text" class="form-control" id="kinerja" value="<?php echo $eks->kinerja; ?>" name="kinerja" required>
+				  </div>
+				  <button type="submit" class="btn btn-primary">Update</button>
+				  <button type="button" onclick="location.href='?page=nilai'" class="btn btn-success">Kembali</button>
+				</form>
+
+		  </div>
+		  </div>
+
+
+<?php
+if($_POST){
+
+	$eks->nuptk = $_POST['nuptk'];
+	$eks->nama = $_POST['nama'];
+	$eks->absen = $_POST['absen'];
+	$eks->komptensi = $_POST['komptensi'];
+	$eks->pendidikan = $_POST['pendidikan'];
+	$eks->kinerja = $_POST['kinerja'];
+
+	if($eks->update()){
+		echo "<script>location.href='?page=nilai'</script>";
+	} else{
+?>
+<div class="alert alert-danger alert-dismissible" role="alert">
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+  <strong>Gagal Ubah Data!</strong> Terjadi kesalahan, coba sekali lagi.
+</div>
+<?php
+	}
+}
+}
+		  ?>
 		  <div class="col-xs-12 col-sm-3 col-md-3">
 		  	<?php include_once 'sidebar.php'; ?>
 		</div>
